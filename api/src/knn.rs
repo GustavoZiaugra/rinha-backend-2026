05e1837b2f5d0a2bc008852e2f2366f7a13059b8
Gustavo Ziaugra <gustavo.ziaugra@live.com>
2026-05-13 11:35:47 +0000

    perf: restore best params WORKERS=6, NPROBE=20, KNN_K=5 for UDS architecture, tag v50

--- api/src/knn.rs
+++ api/src/knn.rs
@@ -2,10 +2,10 @@ use crate::data::Dataset;
 use std::arch::x86_64::*;
 use std::mem::MaybeUninit;
 
-const FULL_NPROBE: usize = 5;
+const FULL_NPROBE: usize = 20;
 const MAX_CENTROIDS: usize = 4096;
 const VECTOR_SCALE: f32 = 0.0001;
-const KNN_K: usize = 3;
+const KNN_K: usize = 5;
 
 pub fn knn5_fraud_count(query: &[f32; 14], ds: &Dataset) -> u8 {
     unsafe { knn5_ivf(query, ds) }
